65718eebe63675805c1b539ae704eb7daa8ae2b9
OGabrielPereira <ogabrielpereira@pm.me>
2022-08-14 16:02:23 -0400

    useState added

---
+++ src/components/Timer/Timer.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const DefaultRamainingTime = {
+    seconds: '00',
+    minutes: '00',
+    hours: '00',
+    days: '00',
+};
+
+export function Timer() {
+    const [remainingTime, setRemainingTime] = useState(DefaultRamainingTime);
+    return (
+        <div className="flex justify-center text-3xl h-screen">
+            <div className="my-96 m-2">
+                <span>{remainingTime.days}</span>
+                <span>d</span>
+            </div>
+            <div className="my-96 m-2">
+                <span>{remainingTime.hours}</span>
+                <span>h</span>
+            </div>
+            <div className="my-96 m-2">
+                <span>{remainingTime.minutes}</span>
+                <span>m</span>
+            </div>
+            <div className="my-96 m-2">
+                <span>{remainingTime.seconds}</span>
+                <span>s</span>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Timer } from "../components/Timer/Timer";
 
 export function Home() {
     return (
         <div>
             <h1 className="flex justify-center text-3xl">Home</h1>
+            <Timer />
         </div>
     );
 }
